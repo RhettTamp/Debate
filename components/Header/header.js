f155b1a684a14cb2bb0b154d0ff3107933557064
tanpei <465821361@qq.com>
2018-08-10 23:46:10 +0800

    不服来辩项目创建

---
+++ components/Header/header.js
@@ -0,0 +1,27 @@
+// components/header/header.js
+Component({
+  /**
+   * 组件的属性列表
+   */
+  properties: {
+      rate:{
+        type:String,
+
+      }
+      
+  },
+
+  /**
+   * 组件的初始数据
+   */
+  data: {
+    avatar:'../../images/1.jpg'
+  },
+
+  /**
+   * 组件的方法列表
+   */
+  methods: {
+
+  }
+})
---
+++ pages/rank/rank.js
@@ -0,0 +1,56 @@
+
+var util = require('../../utils/util.js')
+const app = getApp();
+Page({
+  data: {
+    userInfo:[],
+    currentTab: 0,
+    friendsData: [{
+      openId: 'oOMX50Ar7hO5avGk0QMtMIV43E4Y',
+      nickName: 'bouc',
+      avatarUrl: 'https://wx.qlogo.cn/mmopen/vi_32/Q0j4TwGTfTKrPUCcIM6yLm8ANHYxGjicnsBcXVF5zzicLv8HmjNyZzcwT1Btmlic6Xj3GnlXHLkKNU4d5V9AmKcWg/132',
+      score: 0
+    },
+      {
+        openId: 'oOMX50Ar7hO5avGk0QMtMIV43E4Y',
+        nickName: 'test',
+        avatarUrl: 'https://wx.qlogo.cn/mmopen/vi_32/Uv7icRK28GZ7cne8kfrJxFs3Ew6K509uyXOlIAuCqwSgWbO8mND3NQEIGjLEuvicyle3IsgK2gcXGHquuEluoia6g/132',
+        city: '广州'
+      }],
+    globalData: [
+      {
+        openId: 'oOMX50Ar7hO5avGk0QMtMIV43E4Y',
+        nickName:'bouc',
+        avatarUrl: 'https://wx.qlogo.cn/mmopen/vi_32/Q0j4TwGTfTKrPUCcIM6yLm8ANHYxGjicnsBcXVF5zzicLv8HmjNyZzcwT1Btmlic6Xj3GnlXHLkKNU4d5V9AmKcWg/132',
+        score:0
+      },
+      {
+        openId: 'oOMX50Ar7hO5avGk0QMtMIV43E4Y',
+        nickName: 'bouc',
+        avatarUrl: 'https://wx.qlogo.cn/mmopen/vi_32/Q0j4TwGTfTKrPUCcIM6yLm8ANHYxGjicnsBcXVF5zzicLv8HmjNyZzcwT1Btmlic6Xj3GnlXHLkKNU4d5V9AmKcWg/132',
+        city:'广州'
+      }
+    ],
+    loadNumber: 1//全球排名数据加载次数
+  },
+  onLoad: function () {
+    this.setData({
+      userInfo:app.globalData.userInfo
+    })
+    console.log(this.data.userInfo)
+  },
+  onShow() {
+    
+  },
+  onReachBottom: function () {//下拉加载
+   
+   
+  },
+  
+  swichNav(e) {
+    var that = this;
+    that.setData({
+      currentTab: e.target.dataset.current,
+    })
+  },
+})
